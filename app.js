d8945580bbdb2bd431b589fa969e072c45d76904
arnitkun <ankit.arun@getpostman.com>
2019-08-20 16:58:38 +0530

    weatherApp-05 dynamically get coordinates for given address

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
 
-request({url: 'http://open.mapquestapi.com/geocoding/v1/address?key=1XBOra9jpZAJWg2ayYbc1O8aoak6OOuB&location=bangalore,%20karnataka',
+const KEY = '1XBOra9jpZAJWg2ayYbc1O8aoak6OOuB'; //dude remove this, import it from a gitignored file
+
+const argv = yargs
+    .options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for.',
+        string: true,
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+var encodedAddress = encodeURI(argv.address);
+
+request({url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${encodedAddress}`,
         json: true
     }, (error, response, body) => {
     // console.log(JSON.stringify(body, undefined, 2));
-    console.log(body.results[0].locations[0].latLng);
+    console.log('Address: ', body.results[0].providedLocation.location);
+    console.log('Latitude: ', body.results[0].locations[0].latLng.lat);
+    console.log('Longitude: ', body.results[0].locations[0].latLng.lng);
 });
